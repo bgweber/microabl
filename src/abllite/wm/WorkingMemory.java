49d55c007b99a3e0c4a52dbe8011b47b3a3230f9
bgweber@gmail.com <bgweber@gmail.com@ed98049f-4af1-ae16-196e-a2b4b79dfa29>
2012-05-21 22:07:20 +0000

    Commented

--- src/abllite/wm/WorkingMemory.java
+++ src/abllite/wm/WorkingMemory.java
@@ -2,11 +2,19 @@ package abllite.wm;
 
 import java.util.HashMap;
 import java.util.HashSet;
-
+/**
+ * Maintains lists of working memory elements (WMEs). 
+ */
 public class WorkingMemory {
-	 
-	private HashMap<Class, HashSet<WME>> wmes = new HashMap<Class, HashSet<WME>>();
 
+	/** Maintains lists of WMEs indexed by WME class. **/
+	private HashMap<Class<? extends WME>, HashSet<WME>> wmes = new HashMap<Class<? extends WME>, HashSet<WME>>();
+ 
+	/**
+	 * Adds a WME to working memory. 
+	 * 
+	 * Note: Sets are used in this implementation and duplicate additions are ignored.  
+	 */
 	public void addWME(WME wme) {
 		Class wmeClass = wme.getClass();
 		
@@ -16,12 +24,15 @@ public class WorkingMemory {
 			}
 			
 			wmes.get(wmeClass).add(wme);
-		
-			
+ 		
+			// continue to add the WME to super classes until the base WME class is reached. 
 			wmeClass = wmeClass.getSuperclass();
 		}		
 	}
 
+	/**
+	 * Removes a WME from working memory. 
+	 */
 	public void removeWME(WME wme) {
 		Class wmeClass = wme.getClass();
 		 
@@ -33,7 +44,10 @@ public class WorkingMemory {
 			wmeClass = wmeClass.getSuperclass();
 		}		
 	} 
-	
+
+	/**
+	 * Returns all WMEs of the specified class. 
+	 */
 	public HashSet<WME> getWMEs(Class<? extends WME> wmeClass) {
 		if (!wmes.containsKey(wmeClass)) {
 			wmes.put(wmeClass, new HashSet<WME>());
@@ -42,8 +56,11 @@ public class WorkingMemory {
 		return wmes.get(wmeClass);
 	} 
 
+	/**
+	 * Prints everything in working memory. 
+	 */
 	public void dump() {
-		for (Class wmeClass : wmes.keySet()) {
+		for (Class<? extends WME> wmeClass : wmes.keySet()) {
 			System.out.println(wmeClass.getSimpleName() + ": " + wmes.get(wmeClass));
 		}
 	}
